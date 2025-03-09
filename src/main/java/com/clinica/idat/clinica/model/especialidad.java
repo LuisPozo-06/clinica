9e4b125978395f5b1dc45a9354d55e0b90b78a90
batuneitor <serrott16@gmail.com>
2025-03-08 20:21:23 -0500

    brayantorres

--- src/main/java/com/clinica/idat/clinica/model/especialidad.java
+++ src/main/java/com/clinica/idat/clinica/model/especialidad.java
@@ -1,4 +1,39 @@
 package com.clinica.idat.clinica.model;
 
 public class Especialidad {
+    private int idEspecialidad;
+    private String nombreEspecialidad;
+    private String descripcion;
+
+    public Especialidad() {}
+
+    public Especialidad(int idEspecialidad, String nombreEspecialidad, String descripcion) {
+        this.idEspecialidad = idEspecialidad;
+        this.nombreEspecialidad = nombreEspecialidad;
+        this.descripcion = descripcion;
+    }
+
+    public int getIdEspecialidad() {
+        return idEspecialidad;
+    }
+
+    public void setIdEspecialidad(int idEspecialidad) {
+        this.idEspecialidad = idEspecialidad;
+    }
+
+    public String getNombreEspecialidad() {
+        return nombreEspecialidad;
+    }
+
+    public void setNombreEspecialidad(String nombreEspecialidad) {
+        this.nombreEspecialidad = nombreEspecialidad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
